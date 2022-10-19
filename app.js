9b0c747302d04e277819bbd85673838e2c5ca59c
markos <markos.paltser@voco.ee>
2022-10-19 13:34:51 +0300

    delete books from LocalStorage and read on reload [Delivers #183567774

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const taskList = document.querySelector('table')
 const InPutT = document.getElementById('Title')
 const InPutA= document.getElementById('Author')
 const InPutI= document.getElementById('ISBN#')
+const masterTable = document.querySelector('#books')
+
 
 
 subbtn.addEventListener("click", addbook)
 taskList.addEventListener('click', deleteBook)
+document.addEventListener('DOMContentLoaded', getBooks)
 
 let table = document.getElementById('books')
 
@@ -33,9 +36,31 @@ function deleteBook(e){
     if(e.target.textContent === 'X'){
         if(confirm('Are you sure to delete this task?')){
             e.target.parentElement.parentNode.remove()
-
+            let name = event.target.parentElement.parentElement.children[0].innerText
+            let author = event.target.parentElement.parentElement.children[1].innerText
+            let ISBN = event.target.parentElement.parentElement.children[2].innerText
+            book = [name, author, ISBN]
+            deleteBookLS(book)
         }
     }
+
+}
+function deleteBookLS(book) {
+    let books
+    console.log(book)
+    if(localStorage.getItem('books') === null){
+        books = []
+    } else {
+        books = JSON.parse(localStorage.getItem('books'))
+    }
+    books.forEach((bookLS, booksIndex) => {
+        if(JSON.stringify(bookLS) === JSON.stringify(book)){
+            books.splice(booksIndex, 1)
+        }
+
+    })
+    localStorage.setItem('books', JSON.stringify(books))
+
 }
 
 function addBookLS(title, author, isbn) {
@@ -48,4 +73,30 @@ function addBookLS(title, author, isbn) {
     let book = [title, author, isbn]
     books.push(book)
     localStorage.setItem('books', JSON.stringify(books))
+}
+function getBooks(){
+    let books
+    if(localStorage.getItem('books') === null){
+        books = []
+    } else {
+        books = JSON.parse(localStorage.getItem('books'))
+    }
+    books.forEach((book) => {
+        const getRow = masterTable.insertRow()
+
+        const getCell1 = getRow.insertCell()
+        const getCell2 = getRow.insertCell()
+        const getCell3 = getRow.insertCell()
+        const getCell4 = getRow.insertCell()
+
+        const getCross = document.createElement('a')
+        getCross.appendChild(document.createTextNode('X'))
+        getCross.className = 'red-text text-darken-2 secondary-content'
+        getCross.setAttribute('href', '#')
+
+        getCell1.innerHTML = book[0].toString()
+        getCell2.innerHTML = book[1].toString()
+        getCell3.innerHTML = book[2].toString()
+        getCell4.appendChild(getCross)
+    })
 }
